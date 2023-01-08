dfd61948aacc62abc3dbd7ab25e3d2b5cbee991b
Hexout42 <baskakov.igo@yandex.ru>
2023-01-08 13:20:23 +0300

    already

--- src/main/java/LinksSuggester.java
+++ src/main/java/LinksSuggester.java
@@ -1,13 +1,89 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class LinksSuggester {
+ private final  List<Suggest> suggests = new ArrayList<>();
+List<Suggest> inText ;
 
-    public LinksSuggester(File file) throws IOException, WrongLinksFormatException {
+File file;
+Scanner reader;
+StringBuilder builder =new StringBuilder();
+    ArrayList<String> keyWords = new ArrayList<>();
+    ArrayList<String> titles =new ArrayList<>();
+    ArrayList<String> url = new ArrayList<>();
+
+    public LinksSuggester(File file, Scanner reader) throws IOException, WrongLinksFormatException {
+        this.file = file;
+        this.reader =reader;
+
+
+            createSuggest();
 
     }
+    public List<Suggest> getAllsuggest(){
+        return suggests;
+    }
 
-    public List<Suggest> suggest(String text) {
-        return null;
+    public List<Suggest> suggest(String text)  {
+
+        inText = new ArrayList<>();
+           for (int i =0; i<suggests.size();i++){
+               if( text.indexOf(suggests.get(i).getKeyWord())>0){
+                   inText.add(suggests.get(i));
+               }
+
+           }
+
+           return inText;
+    }
+    public  void createSuggest() {
+        int g;
+
+        while (reader.hasNextLine()){
+
+
+
+    builder.append(reader.nextLine());
+    try {
+
+
+    g = builder.indexOf("\t");
+    if ((g == 0) || (g == -1)) {
+        keyWords.add(null);
+        titles.add(null);
+        url.add(null);
+        throw new WrongLinksFormatException("Неверный формат конфига");
+
+    }
+    keyWords.add(builder.substring(0, g));
+    builder.delete(0, g + 1);
+    g = builder.indexOf("\t");
+        if ((g == 0) || (g == -1)) {
+
+            titles.add(null);
+            url.add(null);
+            throw new WrongLinksFormatException("Неверный формат конфига");
+
+        }
+    titles.add(builder.substring(0, g));
+    builder.delete(0, g + 1);
+    if (builder.length() == 0) {
+        url.add(null);
+        throw new WrongLinksFormatException("Неверный формат конфига");
+
+    }
+    url.add(builder.toString());
+    builder.delete(0, builder.length());
+
+          }catch (WrongLinksFormatException e){
+        System.out.println(e.getMessage());
+        builder.delete(0,builder.length());
+    }}
+        for (int i=0;i<keyWords.size();i++){
+            suggests.add(new Suggest(keyWords.get(i),titles.get(i), url.get(i) ));
+        }
+
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,145 @@
-import java.io.File;
+
+import com.itextpdf.kernel.geom.Rectangle;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.pdf.action.PdfAction;
+import com.itextpdf.kernel.pdf.annot.PdfLinkAnnotation;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
+import com.itextpdf.layout.Canvas;
+
+import com.itextpdf.layout.element.Link;
+import com.itextpdf.layout.element.Paragraph;
+
+import java.io.*;
+import java.util.ArrayList;
+
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        // создаём конфиг
-        LinksSuggester linksSuggester = new LinksSuggester(new File("data/config"));
+    File file = new File("data/config");
+  Scanner reader  =new Scanner(file);
+  List<File> pdfs = new ArrayList<>();
+  List<Suggest> suggestList = new ArrayList<>();
+  ArrayList<PdfDocument> allobject = new ArrayList<>();
+  List<Suggest> allSuggest = new ArrayList<>();
 
-        // перебираем пдфки в data/pdfs
 
-        // для каждой пдфки создаём новую в data/converted
+  File outPdf;
 
-        // перебираем страницы pdf
-
-        // если в странице есть неиспользованные ключевые слова, создаём новую страницу за ней
-
-        // вставляем туда рекомендуемые ссылки из конфига
+    public Main() throws IOException {
     }
+    LinksSuggester linksSuggester = new LinksSuggester(file,reader);
+
+
+    public static void main(String[] args) throws Exception {
+        Main main = new Main();
+
+
+
+        main.pdfInput();
+        main.pdfReader();
+
+
+       }
+
+
+
+    public void pdfInput() throws IOException {
+        var dir = new File("data/pdfs");
+
+        for (var fileIn : dir.listFiles()) {
+
+            pdfs.add(fileIn);
+            outPdf = new File("data/converted/" + fileIn.getName());
+            outPdf.createNewFile();
+            var doc = new PdfDocument(new PdfReader(fileIn), new PdfWriter(outPdf));
+            allobject.add(doc);
+        }
+
+
+    }
+
+
+    public void pdfReader() throws IOException {
+       int allPages;
+        for (int i=0;i<allobject.size();i++){
+           var doc= allobject.get(i);
+           allPages =doc.getNumberOfPages();
+
+           allSuggest = rest(linksSuggester.getAllsuggest());
+
+           for (int j=1;j<=allPages;j++){
+               var text = PdfTextExtractor.getTextFromPage(doc.getPage(j));
+               text.toLowerCase();
+                  suggestList = linksSuggester.suggest(text);
+                                  if ((suggestList.size() > 0)&&(allSuggest.size()>0) && (checkSuggest(suggestList))){
+                                        createNewPage(suggestList,j+1,doc);
+                                        check(allSuggest,suggestList);
+
+                                        allPages+=1;
+                                        j++;
+
+                                  }
+
+           }
+            allobject.get(i).close();
+
+        }
+
+
+    }
+    public void createNewPage( List<Suggest> suggestList,int i,PdfDocument doc){
+
+         var newPage = doc.addNewPage(i);
+         var rect = new Rectangle(newPage.getPageSize()).moveRight(10).moveDown(10);
+        Canvas canvas = new Canvas(newPage, rect);
+      Paragraph paragraph = new Paragraph("Suggestions:\n");
+         paragraph.setFontSize(25);
+         putIntoConfig(suggestList,rect,paragraph);
+         canvas.add(paragraph);  }
+    public void putIntoConfig( List<Suggest> suggestList,Rectangle rect,Paragraph paragraph){
+        StringBuilder builder = new StringBuilder();
+        Link link;
+            for (int i=0;i<suggestList.size();i++){
+                if (allSuggest.indexOf(suggestList.get(i))>-1) {
+                    PdfLinkAnnotation annotation = new PdfLinkAnnotation(rect);
+                    PdfAction action = PdfAction.createURI(suggestList.get(i).getUrl());
+                    annotation.setAction(action);
+                    link = new Link(suggestList.get(i).getTitle(), annotation);
+                    paragraph.add(link.setUnderline());
+                    paragraph.add("\n");
+                }}
+
+    }
+    public  void check (List<Suggest> all, List<Suggest> suggestList){
+        for (int i=0;i<all.size();i++){
+            for (int j=0;j<suggestList.size();j++){
+                if (all.get(i).equals(suggestList.get(j)))
+                    all.remove(i);
+            }
+        }
+    }
+    public boolean checkSuggest(List<Suggest> list){
+        for (int i=0;i<suggestList.size();i++){
+            if (allSuggest.indexOf(suggestList.get(i))>-1)
+                return true;
+    }
+        return false;
+    }
+    public List<Suggest> rest( List<Suggest> link){
+        List<Suggest> all = new ArrayList<>();
+        for (int i=0; i<link.size();i++){
+
+            all.add(link.get(i));
+        }
+        return all;
+    }
+
 }
+
+
+
+
+
